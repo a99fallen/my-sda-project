c03f7db5ef0e13a3313012f618e93e3a8184b67b
fallen_a99 <fallen_a99@o2.pl>
2020-08-29 11:43:57 +0200

    pierwszy update danych

---
+++ projects/dobre-mysli/src/main/java/projects/goodthoughts/GoodThoughtsApp.java
@@ -0,0 +1,12 @@
+package projects.goodthoughts;
+
+import org.slf4j.Logger;        //Simple Login Facade For Java
+import org.slf4j.LoggerFactory;
+
+public class GoodThoughtsApp {
+    private final static Logger log = LoggerFactory.getLogger(GoodThoughtsApp.class);
+
+    public static void main(String args[]) {
+        log.info("Startujemy aplikację! Wziuuummm!!!!");
+    }
+}
